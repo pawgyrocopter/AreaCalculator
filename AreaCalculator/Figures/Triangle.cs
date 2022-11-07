15edf362ad7fc0cdbf11d7478142cd251e814c7b
Savely Volodkov <s.volodkov@syberry.com>
2022-11-07 20:17:21 +0300

    Changed checking right and existence of triangle

--- AreaCalculator/Figures/Triangle.cs
+++ AreaCalculator/Figures/Triangle.cs
@@ -1,22 +1,20 @@
-﻿using AreaCalculator.Exceptions;
+﻿using System.Diagnostics;
+using AreaCalculator.Exceptions;
 
 namespace AreaCalculator.Figures;
 
 public class Triangle : Shape
 {
-    private decimal _firstSide;
-    private decimal _secondSide;
-    private decimal _thirdSide;
+    private decimal _firstSide = 0m;
+    private decimal _secondSide = 0m;
+    private decimal _thirdSide = 0m;
+
+    public readonly bool IsRight = false;
 
     public Triangle(decimal firstSide, decimal secondSide, decimal thirdSide)
     {
-        if (firstSide + secondSide < thirdSide ||
-            secondSide + thirdSide < firstSide ||
-            thirdSide + firstSide < secondSide)
-        {
-            throw new FigureException("This triangle can't exist");
-        }
-
+        CheckExistance(firstSide, secondSide, thirdSide);
+        IsRight = CheckRight(firstSide, secondSide, thirdSide);
         _firstSide = firstSide;
         _secondSide = secondSide;
         _thirdSide = thirdSide;
@@ -25,6 +23,31 @@ public class Triangle : Shape
     public override decimal CalculateArea()
     {
         var p = (_firstSide + _secondSide + _thirdSide) / 2;
-        return (decimal)Math.Sqrt((double) (p * (p - _firstSide) * (p - _secondSide) * (p - _thirdSide)));
+        return (decimal) Math.Sqrt((double) (p * (p - _firstSide) * (p - _secondSide) * (p - _thirdSide)));
+    }
+
+    private bool CheckExistance(in decimal firstSide,in decimal secondSide,in decimal thirdSide)
+    {
+        if (firstSide + secondSide < thirdSide ||
+            secondSide + thirdSide < firstSide ||
+            thirdSide + firstSide < secondSide)
+        {
+            throw new FigureException("This triangle can't exist");
+        }
+        return true;
+    }
+
+    private bool CheckRight(in decimal firstSide, in decimal secondSide, in decimal thirdSide)
+    {
+        var maxSide = Math.Max(Math.Max(firstSide, secondSide), thirdSide);
+        var right = maxSide switch
+        {
+            var value when value == firstSide => secondSide * secondSide * secondSide + thirdSide * thirdSide == firstSide * firstSide ,
+            var value when value == secondSide => firstSide * firstSide + thirdSide * thirdSide == secondSide * secondSide ,
+            var value when value == thirdSide => secondSide * secondSide + firstSide * firstSide == thirdSide * thirdSide ,
+            _ => false
+        };
+
+        return right;
     }
 }
